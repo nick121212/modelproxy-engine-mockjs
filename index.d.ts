d2d160d271e48d65358b13f651cad1ce2c868a21
封晓 <NICK@fengxiaodeMacBook-Pro.local>
2017-03-14 11:47:54 +0800

    dd

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,13 @@
-declare module 'modelproxy' {
+import { ModelProxy, modelProxy } from 'modelproxy';
+
+declare module 'modelproxy-engine-mockjs' {
     export namespace ModelProxyEngine {
-        class MockEngine extends BaseEngine {
-            constructor();
+         class MockEngine implements ModelProxy.IEngine {
+            constructor(mockEngine?: ModelProxy.IEngine);
             validate(intance: ModelProxy.IInterfaceModel, options: ModelProxy.IExecute): boolean;
             proxy(intance: ModelProxy.IInterfaceModel, options: ModelProxy.IExecute): Promise<any>;
         }
     }
+
+    export var MockEngine: typeof ModelProxyEngine.MockEngine;
 }
